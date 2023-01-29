4cb99276750f1b9caa1af6852b660fd03811f62a
jwong65 <jason.wong6507@gmail.com>
2023-01-29 12:20:53 -0500

    Added styled example1 component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Example1 from './components/example1'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Example1></Example1>
     </div>
   );
 }
---
+++ src/components/example1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const styledText = {
+    fontSize: "25px",
+    textAlign: "center",
+    margin: "40px",
+    border: "1px solid green"
+}
+export default function example1() {
+  return (
+    <div style ={styledText}>example1</div>
+  )
+}
